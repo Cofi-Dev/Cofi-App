0aad5c11aab545847cfae39c728501f893113395
victorst79 <victorst79@gmail.com>
2021-02-03 08:56:42 +0100

    rework project in react/ts

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const appName: String  = 'NaM-App'
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>{appName}</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
