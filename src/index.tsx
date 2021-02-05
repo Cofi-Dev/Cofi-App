f0db837ecb8b498fb9a1c3f811421dd1662c4b9f
victorst79 <victorst79@gmail.com>
2021-02-05 18:05:24 +0100

    settings and some stuffs

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { Route, Redirect } from 'react-router-dom'
+
+import {Home} from './views/home'
+import {About} from './views/about'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Redirect to='/' />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
