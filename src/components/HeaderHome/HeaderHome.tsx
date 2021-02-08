55710b3c5abc60cd0b0cc56d3ee23c49dce575ee
victorst79 <victorst79@gmail.com>
2021-02-08 21:31:40 +0100

    working in nav hook

--- src/components/HeaderHome/HeaderHome.tsx
+++ src/components/HeaderHome/HeaderHome.tsx
@@ -1,14 +1,16 @@
 import React, { ReactElement } from "react";
-
 import { useTranslation } from 'react-i18next';
+import './HeaderHome.scss'
 
 const NamHeaderHome: React.FC = (): ReactElement => {
     const {t} = useTranslation ();
 
     return (
-        <div>
-            <p>{t("title")}</p>
-        </div>
+        <nav className="nam-nav-bar">
+            <div className="nam-nav-bar__inner">
+                 <p>{t("title")}</p>
+            </div>
+        </nav>
     )    
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,13 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './plugins/i18n';
-import { BrowserRouter } from 'react-router-dom';
-import { Route, Redirect } from 'react-router-dom'
 
-import {Home} from './views/home'
-import {About} from './views/about'
+import AppRouter from './router/'
 
 ReactDOM.render(
   <React.StrictMode>
     <Suspense fallback={<div>Loading...</div>}>
-      <BrowserRouter>
-          <Route path='/' exact component={Home} />
-          <Route path='/about' component={About} />
-          <Redirect to='/' />
-      </BrowserRouter>
+      <AppRouter />
     </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
